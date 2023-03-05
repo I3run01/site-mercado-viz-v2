b1d10d61998a76ee6838331cc126cbd30e13c74c
I3run01 <brunnooa.v@gmail.com>
2023-03-05 15:09:06 -0300

    c

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Menubar } from './components/menubar'
+import { Cape } from './components/Cape'
 import { GlobalStyle } from './Global'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <GlobalStyle>
       <Menubar/>
+      <Cape/>
     </GlobalStyle>
   )
 }
--- src/components/Cape/index.tsx
+++ src/components/Cape/index.tsx
@@ -4,7 +4,7 @@ export const Cape = () => {
 
     return (
         <CapeStyled>
-
+            
         </CapeStyled>
     )
 }
