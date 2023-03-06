984c1c07274502fee586620ced8fb70ae14a0ae4
I3run01 <brunnooa.v@gmail.com>
2023-03-06 19:51:59 -0300

    c

--- src/components/Cape/index.tsx
+++ src/components/Cape/index.tsx
@@ -4,7 +4,12 @@ export const Cape = () => {
 
     return (
         <CapeStyled>
-            
+            <img src='Cape images/products.png' alt="" id='productsIMG'/>
+            <img src="Cape images/Store symbol.png" alt="" id='storeSymbol'/>
+            <div id='title'>
+                <h1>Mercado</h1>
+                <h1>Vizinhança</h1>
+            </div>
         </CapeStyled>
     )
 }
--- src/components/Cape/style.tsx
+++ src/components/Cape/style.tsx
@@ -1,6 +1,34 @@
 import styled from "styled-components";
 
+const fontColor = 'black'
+
 export const CapeStyled = styled.div`
+    @import url('https://fonts.googleapis.com/css2?family=Dosis:wght@700&display=swap');
+
+    height: 100vh;
+    position: relative;
+    overflow-x: hidden;
+
+    #productsIMG {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+    }
+
+    #storeSymbol {
+        position: absolute;
+        top: 95px;
+        width: 250px;
+        left: 20px;
+    }
     
+    #title {
+        position: absolute;
+        top: 95px;
+        left: 50px;
+        font-size: 4px;
+        font-family: 'Dosis';
+        color: ${fontColor};
+    }
     
 `
