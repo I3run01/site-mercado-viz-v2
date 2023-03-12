a1cd83a2696a505aeaae360b319028d656e1e043
I3run01 <brunnooa.v@gmail.com>
2023-03-12 08:31:42 -0300

    c

--- src/components/Cape/index.tsx
+++ src/components/Cape/index.tsx
@@ -4,13 +4,16 @@ export const Cape = () => {
 
     return (
         <CapeStyled>
-            <img src='Cape images/products.png' alt="" id='productsIMG'/>
-            <img src="Cape images/Store symbol.png" alt="" id='storeSymbol'/>
+            <div id='logo'>
+                <img src="Cape images/Store symbol.png" alt="" id='storeSymbol'/>
+                <h1 id='title'>Mercado <br/> Vizinhança</h1>
+            </div>
             <img src="Cape images/elipse bk.png" alt="" id='elipseBK'/>
-            <h1 id='title'>Mercado <br/> Vizinhança</h1>
             <p id='paragraph'>
                 Bem-vindo ao Mercado Vizinhança! Oferecemos uma variedade de produtos de alta qualidade, incluindo carne fresca, pão e frutas e verduras locais. Além disso, temos itens de higiene pessoal e produtos de limpeza. Visite-nos ou navegue pelo nosso site para saber mais!
             </p>
+            
+            <img src='Cape images/products.png' alt="" id='productsIMG'/>
         </CapeStyled>
     )
 }
--- src/components/Cape/style.tsx
+++ src/components/Cape/style.tsx
@@ -5,53 +5,59 @@ const fontColor = 'black'
 export const CapeStyled = styled.div`
     @import url('https://fonts.googleapis.com/css2?family=Dosis:wght@700&display=swap');
 
-    height: 100vh;
+    min-height: 100vh;
     position: relative;
     overflow: hidden;
 
-    #productsIMG {
-        position: absolute;
-        z-index: +1;
-        bottom: -100px;
-        width: 100%;
-        scale: 110%;
-    }
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
 
-    #storeSymbol {
-        position: absolute;
-        z-index: +1;
-        top: 60px;
-        width: 250px;
-        left: 20px;
+    padding-top: 80px;
+
+    #logo {
+        display: flex;
+        flex-direction: row;
+        justify-content: start;
+        align-items: center;
+
+        #storeSymbol {
+            z-index: +1;
+            width: 250px;
+            scale: 90%;
+        }
+        
+        #title {
+            z-index: +1;
+            font-size: 36px;
+            font-family: 'Dosis';
+            color: ${fontColor};
+            margin-left: 40px;
+        }
     }
     
-    #title {
-        position: absolute;
-        z-index: +1;
-        top: 120px;
-        left: 340px;
-        font-size: 36px;
-        font-family: 'Dosis';
-        color: ${fontColor};
-    }
-
     #paragraph {
-        position: absolute;
         z-index: +1;
-        top: 280px;
-        left: 50px;
         width: 500px;
         font-size: 16px;
         font-family: 'Dosis';
         font-weight: bold;
         color: ${fontColor};
+
+        padding-left: 40px;
+    }
+
+    #productsIMG {
+        z-index: +1;
+        width: 100%;
+        scale: 110%;
     }
 
     #elipseBK {
-        z-index: 0;
         position: absolute;
+        z-index: 0;
         top: 0;
         opacity: .6;
     }
     
-`
+    `
